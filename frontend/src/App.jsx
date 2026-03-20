a62dd79506377f4cbc260f79129f7d8997cc1247
JuanmaM-G <kporta715@gmail.com>
2026-03-20 08:44:45 -0500

    si

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/login';
-import Registro from './components/registro';
-import Index from './components/index';
+import Login from './components/Login';
+import Registrarse from './components/Registro';
+import Index from './components/Index';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/"            element={<Login />} />
         <Route path="/login"       element={<Login />} />
-        <Route path="/registro" element={<Registro />} />
-        <Route path="/index"    element={<Index />} />
+        <Route path="/registrarse" element={<Registrarse />} />
+        <Route path="/trabajos"    element={<Index />} />
       </Routes>
     </BrowserRouter>
   );
